9a87b40436e3eb8f341e70e5587a2eb34c3f2ca6
Miguel Ferreira <miguel.r.ferreira@ua.pt>
2021-11-27 15:18:56 +0000

    ready for first evaluation

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ echo 'Running Bot $1 time(s)'
 i=0
 while [ $i -le $1 ]
 do
-  python3 client.py #>> scores.txt
+  python3 student.py #>> scores.txt
   sleep 1 
   ((i++))
 done
-do python3 media.py
+python3 media.py
